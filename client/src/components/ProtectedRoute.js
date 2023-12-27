a367a8c49fbcbfa8969e651efb65fee29f7f4e73
Aaditya Pandey <aadityapandey0611@gmail.com>
2023-12-28 01:35:30 +0530

    fix some issues bnsdbhdv zmvn xcnx

---
+++ client/src/components/ProtectedRoute.js
@@ -0,0 +1,77 @@
+import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { GetLoggedInUserDetails } from '../apicalls/users';
+import { message } from 'antd';
+import { useDispatch, useSelector } from 'react-redux';
+import { SetUser } from '../redux/usersSlice';
+import { HideLoading, ShowLoading } from '../redux/loadersSlice';
+
+function ProtectedRoute({ children }) {
+  const navigate = useNavigate();
+  const { user } = useSelector(state => state.users)
+  const dispatch = useDispatch();
+
+
+  const validateUserToken = async () => {
+    try {
+      dispatch(ShowLoading())
+      const response = await GetLoggedInUserDetails();
+      dispatch(HideLoading())
+      if (response.success) {
+        dispatch(SetUser(response.data));
+      } else {
+        localStorage.removeItem("token");
+        navigate("/login");
+        message.error(response.message);
+      }
+    } catch (error) {
+      localStorage.removeItem("token");
+      navigate("/login");
+      dispatch(HideLoading())
+      message.error(error.message);
+    }
+  };
+
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (!token) {
+      navigate("/login");
+    }
+    else {
+      validateUserToken();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+  return (
+    <div>
+      {user && (
+        <div className="p-1">
+          <div className="header p-2 bg-primary flex justify-between rounded items-center">
+            <h1 className="text-2xl text-white font-bold cursor-pointer"
+              onClick={() => navigate("/")}
+            >AADI LIBRARY</h1>
+
+            <div className="flex items-center gap-1 bg-white p-1 rounded">
+              <i className="ri-shield-user-fill"></i>
+              <span className="text-sm underline"
+                onClick={() => navigate("/profile")}
+              >
+                {user.name.toUpperCase()}</span>
+              <i className="ri-logout-circle-r-fill ml-2"
+                onClick={() => {
+                  localStorage.removeItem("token");
+                  navigate("/login");
+                }}
+              ></i>
+            </div>
+          </div>
+
+          <div className="content mt-1">{children}</div>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default ProtectedRoute;
---
+++ server/routes/usersRoute.js
@@ -0,0 +1,146 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../models/usersModel");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const authMiddleware = require("../middlewares/authMiddleware");
+
+//new user
+router.post("/register", async (req, res) => {
+    try {
+        //already registered
+        const user = await User.findOne({ email: req.body.email });
+        if (user) {
+            return res.send({
+                success: false,
+                message: "Email already exists",
+            });
+        }
+
+        //hash password
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(req.body.password, salt);
+        req.body.password = hashedPassword;
+
+        //new user
+        const newUser = await User(req.body);
+        await newUser.save();
+        return res.send({
+            success: true,
+            message: "User created successfully , please login",
+        });
+    } catch (error) {
+        res.send({
+            success: false,
+            message: error.message,
+        });
+    }
+});
+
+//login
+router.post("/login", async (req, res) =>{
+    try{
+        //user exist or not
+        const user = await User.findOne({email: req.body.email });
+        if(!user) {
+            return res.send({
+                success : false,
+                message : "User does not exist",
+            });
+        }
+
+        //password is correct
+        const validPassword = await bcrypt.compare(
+            req.body.password,
+            user.password
+        )
+
+        if(!validPassword){
+            return res.send({
+                success : false,
+                message : "Invalid Password",
+            });
+        }
+
+        //creating and assigning token
+        const token = jwt.sign({userId : user._id}, process.env.jwt_secret , {expiresIn : "1d"});
+        return res.send({
+            success : true,
+            message : "Login Successful",
+            data : token
+        });
+
+    }
+    catch(error){
+        return res.send({
+            success : false,
+            message : error.message,
+        });
+    }
+});
+
+//get logged in user details
+router.get("/get-logged-in-user", authMiddleware ,  async (req, res) => {
+    try {
+        const user = await User.findById(req.body.userIdFromToken);
+        if (!user){
+            return res.send({
+                success : false,
+                message : "User does not exist",
+            });
+        }
+        return res.send({
+                success : true,
+                message : "User details fetched successfully",
+                data : user,
+            });
+    } catch (error) {
+        return res.send({
+            success : false,
+            message : error.message,
+        });
+    }
+});
+
+//get all users(students)
+router.get("/get-all-users/:role", authMiddleware, async (req, res) => {
+    try {
+      const users = await User.find({ role: req.params.role }).sort({createdAt: -1,});
+      return res.send({
+        success: true,
+        message: "Users fetched successfully",
+        data: users,
+      });
+    } catch (error) {
+      return res.send({
+        success: false,
+        message: error.message,
+      });
+    }
+  });
+
+module.exports = router;
+
+// get user by id
+router.get("/get-user-by-id/:id", authMiddleware, async (req, res) => {
+    try {
+      const user = await User.findById(req.params.id);
+      if (!user) {
+        return res.send({
+          success: false,
+          message: "User does not exist",
+        });
+      }
+      return res.send({
+        success: true,
+        message: "User fetched successfully",
+        data: user,
+      });
+  
+    } catch (error) {
+      return res.send({
+        success: false,
+        message: 'User does not exist',
+      });
+    }
+  });
